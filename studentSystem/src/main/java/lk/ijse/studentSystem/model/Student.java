7c29a376397e023cccd0fec496a05ab77023d11c
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-06-25 00:02:19 +0530

    Add Student model class

--- studentSystem/src/main/java/lk/ijse/studentSystem/model/Student.java
+++ studentSystem/src/main/java/lk/ijse/studentSystem/model/Student.java
@@ -1,4 +1,25 @@
 package lk.ijse.studentSystem.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //id eka auto genarate venna
+    private int id;
+    private String name;
+    private String address;
+
 }
