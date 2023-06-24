f586a3c7066dd8c015d2ca37393fba3999fa9c84
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-06-24 23:44:29 +0530

    first commit

---
+++ studentSystem/src/main/java/lk/ijse/studentSystem/StudentSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.studentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSystemApplication.class, args);
+	}
+
+}
---
+++ studentSystem/src/test/java/lk/ijse/studentSystem/StudentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.studentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
