072e7617c18236ebc2873f9481bc439dbb2bda58
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-06-25 09:58:06 +0530

    Add StudentRepository class

--- studentSystem/src/main/java/lk/ijse/studentSystem/repository/StudentRepository.java
+++ studentSystem/src/main/java/lk/ijse/studentSystem/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package lk.ijse.studentSystem.repository;
 
-public interface StudentRepository {
+import lk.ijse.studentSystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
