06047fed193bb797ee3b2cf391fd35da1ce80891
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-06-27 23:19:13 +0530

    add BookRepository

--- Book-Catalog-BackEnd-main/src/main/java/com/DataIntimate/BookCatalog/repository/BookRepository.java
+++ Book-Catalog-BackEnd-main/src/main/java/com/DataIntimate/BookCatalog/repository/BookRepository.java
@@ -1,9 +1,21 @@
 package com.DataIntimate.BookCatalog.repository;
 
 import com.DataIntimate.BookCatalog.model.Book;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book,Integer> {
+    @Query(value = "select * from books where book_name=?1", nativeQuery = true)
+    Book findByBookName(String bookName);
+
+    @Query(value = "select * from books where book_id=?1", nativeQuery = true)
+    Book findById(int bookId);
+
+    @Query(value = "select * from books where book_name like %?1% and category like %?2%", nativeQuery = true)
+    List<Book> booksFilter(String bookName, String category);
 }
